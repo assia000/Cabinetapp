828eb127e53ee09a5fa03a2f4b3b7c4e83042bfe
Assia <oumerassia@gmail.com>
2022-04-27 23:44:11 +0200

    Updates

---
+++ precache-manifest.77408fb1e7599ad1468dd9fcf5d4f0fa.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "6813f5df42eaae808c64",
+    "url": "/Cabinetapp/static/css/main.15770cbe.chunk.css"
+  },
+  {
+    "revision": "6813f5df42eaae808c64",
+    "url": "/Cabinetapp/static/js/main.6813f5df.chunk.js"
+  },
+  {
+    "revision": "11d49e5c2f400c0ca2b6",
+    "url": "/Cabinetapp/static/js/1.11d49e5c.chunk.js"
+  },
+  {
+    "revision": "24fb889297f17dd2ee4e",
+    "url": "/Cabinetapp/static/js/2.24fb8892.chunk.js"
+  },
+  {
+    "revision": "aac146fcfdd2782f982e",
+    "url": "/Cabinetapp/static/js/3.aac146fc.chunk.js"
+  },
+  {
+    "revision": "dc745c2f199ae145b88d",
+    "url": "/Cabinetapp/static/js/4.dc745c2f.chunk.js"
+  },
+  {
+    "revision": "154a5cd874099aebc8fa",
+    "url": "/Cabinetapp/static/js/5.154a5cd8.chunk.js"
+  },
+  {
+    "revision": "6627c259f918e6b1e804",
+    "url": "/Cabinetapp/static/js/runtime~main.6627c259.js"
+  },
+  {
+    "revision": "f9556c2074ea7e3e2a471fe1fa52aab4",
+    "url": "/Cabinetapp/static/media/image2.f9556c20.jpg"
+  },
+  {
+    "revision": "07b1a5e9248d596877c03cb7c948c84a",
+    "url": "/Cabinetapp/static/media/image3.07b1a5e9.jpg"
+  },
+  {
+    "revision": "fb31fc9a94cb2e10e7c3acfff91aa9fe",
+    "url": "/Cabinetapp/static/media/logo.fb31fc9a.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Cabinetapp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2f947159c300b3330329ef62f621e022",
+    "url": "/Cabinetapp/static/media/pellicule-facette-dentaire.2f947159.jpg"
+  },
+  {
+    "revision": "fa3c3bc442b555a9a45a7e9c5599558e",
+    "url": "/Cabinetapp/static/media/esthetique.fa3c3bc4.jpg"
+  },
+  {
+    "revision": "f7173619d48690ac90494475f281020a",
+    "url": "/Cabinetapp/static/media/blanchiment-dentaire-femme-sourire-400x378.f7173619.jpg"
+  },
+  {
+    "revision": "4f9fe4eb1efce42faee60276d0900887",
+    "url": "/Cabinetapp/static/media/esthetique-profil-droit.4f9fe4eb.jpg"
+  },
+  {
+    "revision": "5b7e483b76d068744588ad18a528ee6e",
+    "url": "/Cabinetapp/static/media/rehabilitation.5b7e483b.jpg"
+  },
+  {
+    "revision": "f9e85eb6b8d045f1c709c71577f9d0a6",
+    "url": "/Cabinetapp/static/media/DIGITAL_SMILE_DESIGN-400x225.f9e85eb6.jpg"
+  },
+  {
+    "revision": "bcc0f107c38ca913d9a209037d0ddd78",
+    "url": "/Cabinetapp/static/media/apropos2.bcc0f107.jpg"
+  },
+  {
+    "revision": "218bc24506a3512d8a7e9f7a60f5c034",
+    "url": "/Cabinetapp/static/media/apropos.218bc245.jpg"
+  },
+  {
+    "revision": "58d7119eaaa861d4fa808a5991a1e629",
+    "url": "/Cabinetapp/static/media/image1.58d7119e.jpg"
+  },
+  {
+    "revision": "ad381dfae0bd902b10ecd179f69beb3c",
+    "url": "/Cabinetapp/static/media/Endodoncia.ad381dfa.jpg"
+  },
+  {
+    "revision": "0d51fe4574a8ba2b1f0bbffd6f57d13e",
+    "url": "/Cabinetapp/static/media/Plombage-dentaire.0d51fe45.jpg"
+  },
+  {
+    "revision": "a4760751efc767d606fab91c755e40ae",
+    "url": "/Cabinetapp/static/media/inlay.a4760751.jpg"
+  },
+  {
+    "revision": "ddb1a6f1c4bb0c4a2aff748f4ff6f1cb",
+    "url": "/Cabinetapp/static/media/couronne-dentaire-prothese-amovible.ddb1a6f1.jpg"
+  },
+  {
+    "revision": "aa86be90030743a85caf02108e7c9de9",
+    "url": "/Cabinetapp/static/media/brige-dentaire-prothese-amovible.aa86be90.jpg"
+  },
+  {
+    "revision": "e8d93ea0edbb8da054e6bd6a749e035d",
+    "url": "/Cabinetapp/static/media/Orthodontie-1.e8d93ea0.jpg"
+  },
+  {
+    "revision": "a80eee9b428dab676cfc1152d6e82873",
+    "url": "/Cabinetapp/static/media/Invisalign.a80eee9b.jpg"
+  },
+  {
+    "revision": "929cf683814206621bf125ee37e3945a",
+    "url": "/Cabinetapp/static/media/gingivite-medicale.929cf683.jpg"
+  },
+  {
+    "revision": "9489e9e25d079d66ef5049a7a910ee48",
+    "url": "/Cabinetapp/static/media/rehabilitation-complete-sourire.9489e9e2.jpg"
+  },
+  {
+    "revision": "0956ec430fb60bf67217225c5e1bd95c",
+    "url": "/Cabinetapp/static/media/pedodonti-1.0956ec43.jpg"
+  },
+  {
+    "revision": "96ee3eb4c7ae53b4966ba9bc22ef7cfd",
+    "url": "/Cabinetapp/index.html"
+  }
+];
